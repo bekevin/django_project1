8caaed512f34b8f63dcb2936482e63cf3ca98ee5
bekevin <kevinzettel@infoblox.com>
2020-02-09 06:27:27 -0800

    added blog app and added django templates

--- django_project1/blog/views.py
+++ django_project1/blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'kevin',
+        'title': 'blog post 1',
+        'content': 'First post contnet',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'john doe',
+        'title': 'blog post 2',
+        'content': 'second post contnet',
+        'date_posted': 'August 29, 2018'
+    }
+]
 
 # Create your views here. How we want to handle routes.
 def home(request):
-        return HttpResponse('<h1>Blog Home</h1>')
+        context = {
+            'posts': posts
+        }
+        return render(request, 'blog/home.html', context)
 
 def about(request):
-        return HttpResponse('<h1>Blog About</h1>')
+        return render(request, 'blog/about.html', {'title': "About"})
+
+
+
+
+
+
